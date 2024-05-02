326431dda5ee9c8ebe8926ebabdb7c84ff4f58c5
sohamganatra <sohamganatra1@gmail.com>
2024-05-02 15:28:36 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,7 +5,8 @@ pip install poetry
 poetry install
 
 # Add Ngrok repository key and source
-curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null
-echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | sudo tee /etc/apt/sources.list.d/ngrok.list
-sudo apt update
-sudo apt install ngrok
+#curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null
+#echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | sudo tee /etc/apt/sources.list.d/ngrok.list
+#sudo apt update
+#sudo apt install ngrok
+echo "Please install ngrok using link https://ngrok.com/download"
